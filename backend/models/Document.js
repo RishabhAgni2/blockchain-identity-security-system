2b8f08c4f7434e64c5979a48af1c3c55802946e1
RishabhAgni2 <rishabhagni68107@gmail.com>
2026-02-08 22:59:09 +0530

    feat: add document model schema

---
+++ backend/models/Document.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const documentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    originalName: {
+      type: String,
+      required: true,
+    },
+    filePath: {
+      type: String,
+      required: true,
+    },
+    fileHash: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Document", documentSchema);
