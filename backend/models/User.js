817eb5d6bc04855662fd2b5eb005f407917fa452
RishabhAgni2 <rishabhagni68107@gmail.com>
2026-02-08 20:36:56 +0530

    refactor: enhance user model schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,23 +2,15 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
     email: {
       type: String,
       required: true,
       unique: true,
+      lowercase: true,
     },
     password: {
       type: String,
       required: true,
-    },
-    role: {
-      type: String,
-      enum: ["user", "verifier", "admin"],
-      default: "user",
     },
   },
   { timestamps: true }
